73821c5574cc5e08c7e4120100d5b9e1b6f737da
CHANDRU M <chandrumohan550@gmail.com>
2026-08-13 21:14:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Floor in BST/floor-in-bst.java
@@ -0,0 +1,32 @@
+/*
+Definition for Node
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    public int findMaxFork(Node root, int k) {
+        int floor=-1;
+        while(root!=null){
+            if(root.data==k){
+                floor=root.data;
+                return floor;
+            }
+            if(root.data>k){
+                root=root.left;
+            }
+            else{
+                floor=root.data;
+                root=root.right;
+            }
+        }
+        return floor;
+    }
+}
